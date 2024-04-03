84685e4f2166c54da98f89229011fa17e8611694
Heitor Aparecido <heitor.ap.silva98@gmail.com>
2024-04-03 19:06:41 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useRef } from 'react';
+import {
+  ImageURISource,
+  // Pressable,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  ViewToken,
+} from 'react-native';
+import Animated, {
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from 'react-native-reanimated';
+import ListItem from './components/listItem';
+import PaginationElement from './components/pagination';
+import Button from './components/button';
+
+const pages = [
+  {
+    text: 'Tela 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: require('./assets/criatividade.jpg'),
+  },
+  {
+    text: 'Tela 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: require('./assets/building.jpg'),
+  },
+  {
+    text: 'Tela 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: require('./assets/qualidade.jpg'),
+  },
+];
 
 export default function App() {
+  const x = useSharedValue(0);
+  const flatListIndex = useSharedValue(0);
+  const flatListRef = useAnimatedRef<
+    Animated.FlatList<{
+      text: string;
+      image: ImageURISource;
+    }>
+  >();
+
+  const onViewableItemsChanged = useCallback(
+    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      flatListIndex.value = viewableItems[0].index ?? 0;
+    },
+    []
+  );
+  const scrollHandle = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      x.value = event.contentOffset.x;
+    },
+  });
+
+  const renderItem = useCallback(
+    ({
+      item,
+      index,
+    }: {
+      item: { text: string; image: ImageURISource };
+      index: number;
+    }) => {
+      return <ListItem item={item} index={index} x={x} />;
+    },
+    [x]
+  );
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Animated.FlatList
+        ref={flatListRef}
+        onScroll={scrollHandle}
+        horizontal
+        scrollEventThrottle={16}
+        pagingEnabled={true}
+        data={pages}
+        keyExtractor={(_, index) => index.toString()}
+        bounces={false}
+        renderItem={renderItem}
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={onViewableItemsChanged}
+      />
+      <View style={styles.bottomContainer}>
+        <PaginationElement length={pages.length} x={x} />
+        <Button
+          currentIndex={flatListIndex}
+          length={pages.length}
+          flatListRef={flatListRef}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
   },
 });
---
+++ components/button.tsx
@@ -0,0 +1,105 @@
+import {
+    // ImageURISource,
+    Pressable,
+    StyleSheet,
+    // Text,
+    // View,
+  } from 'react-native';
+  import React, { useCallback } from 'react';
+  import Animated, {
+    useAnimatedStyle,
+    SharedValue,
+    withSpring,
+    withTiming,
+  } from 'react-native-reanimated';
+
+  type Props = {
+    currentIndex: SharedValue<number>;
+    length: number;
+    flatListRef: any;
+  };
+  const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+  
+  const Button = ({ currentIndex, length, flatListRef }: Props) => {
+    const rnBtnStyle = useAnimatedStyle(() => {
+      return {
+        width:
+          currentIndex.value === length - 1 ? withSpring(140) : withSpring(60),
+        height: 60,
+      };
+    }, [currentIndex, length]);
+  
+    const rnTextStyle = useAnimatedStyle(() => {
+      return {
+        opacity:
+          currentIndex.value === length - 1 ? withTiming(1) : withTiming(0),
+        transform: [
+          {
+            translateX:
+              currentIndex.value === length - 1 ? withTiming(0) : withTiming(100),
+          },
+        ],
+      };
+    }, [currentIndex, length]);
+  
+    const imageAnimatedStyle = useAnimatedStyle(() => {
+      return {
+        opacity:
+          currentIndex.value !== length - 1 ? withTiming(1) : withTiming(0),
+        transform: [
+          {
+            translateX:
+              currentIndex.value !== length - 1 ? withTiming(0) : withTiming(100),
+          },
+        ],
+      };
+    }, [currentIndex, length]);
+  
+    const onPress = useCallback(() => {
+      if (currentIndex.value === length - 1) {
+        console.log('Get Started');
+        return;
+      } else {
+        flatListRef?.current?.scrollToIndex({
+          index: currentIndex.value + 1,
+        });
+      }
+    }, []);
+    return (
+      <AnimatedPressable style={[styles.container, rnBtnStyle]} onPress={onPress}>
+        <Animated.Text style={[styles.textStyle, rnTextStyle]}>
+          Get Started
+        </Animated.Text>
+        <Animated.Image
+          source={require('../assets/arrow-right.png')}
+          style={[styles.imageStyle, imageAnimatedStyle]}
+        />
+      </AnimatedPressable>
+    );
+  };
+  
+  export default Button;
+  
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      paddingHorizontal: 24,
+      paddingVertical: 16,
+      borderRadius: 100,
+      backgroundColor: '#304FFE',
+      alignItems: 'center',
+      justifyContent: 'center',
+      overflow: 'hidden',
+    },
+    textStyle: {
+      color: 'white',
+      position: 'absolute',
+      fontWeight: '600',
+      fontSize: 16,
+    },
+    imageStyle: {
+      width: 24,
+      height: 24,
+      position: 'absolute',
+    },
+  });
